aa4a1ca02929ec84ec7bb507044a60b9df7f8ee1
rh60 <hlavickar@gmail.com>
2019-02-21 17:04:01 +0100

    long

--- MMP/MMP/Algebra.cs
+++ MMP/MMP/Algebra.cs
@@ -3,24 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using int_t = System.Int64; 
 
 namespace MELib
 {
     public class Binomial
     {
-        int[] p;
-        public int[] Next()
+        int_t[] p;
+        public int_t[] Next()
         {
             if (p == null)
             {
-                p = new int[] { 1 };
+                p = new int_t[] { 1 };
             }
             else
             {
-                var q = new int[p.Length + 1];
+                var q = new int_t[p.Length + 1];
                 q[0] = 1;
                 q[p.Length] = 1;
-                for (int j = 1; j < p.Length; j++)
+                for (int_t j = 1; j < p.Length; j++)
                     q[j] = p[j - 1] + p[j];
                 p = q;
             }
