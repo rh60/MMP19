f7de5926aecf8e653ff64389c064851f16cb7a6c
rh60 <32161865+rh60@users.noreply.github.com>
2019-03-13 13:51:27 +0100

    Add files via upload

--- MMP/queries/generics/Program.cs
+++ MMP/queries/generics/Program.cs
@@ -7,12 +7,16 @@ using number = Generic.Float64.Scalar;
 
 namespace Generic
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
             var x = new Vector<number>(1, 2, 3);
             var norm2 = x.Dot(x);
+
+            var T = new TriStorage<number>(5);
+            T[2, 1] = 1;
         }
     }
 }
---
+++ MMP/queries/generics/TriStorage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    [DebuggerDisplay("{data}")]
+    public class TriStorage<T>
+    {
+        T[][] data;        
+        public TriStorage(int n)
+        {
+            data = new T[n][];
+            for (int i = 0; i < n; i++)
+            {
+                data[i] = new T[i + 1];
+            }
+        }
+
+        public T this[int i, int j]
+        {
+            get
+            {
+                return data[i][j];
+            }
+            set
+            {
+                data[i][j] = value;
+            }
+        }
+    }
+}
